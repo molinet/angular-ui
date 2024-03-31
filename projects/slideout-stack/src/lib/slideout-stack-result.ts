a50f464028184b30f93fc8c20049207c0c02c9bb
David Molinet <molinet.develop@gmail.com>
2024-03-31 19:58:29 +0200

    feat(slideout-stack): Added enhancement for passing data to the 'pop' method.

---
+++ projects/slideout-stack/src/lib/slideout-stack-result.ts
@@ -0,0 +1,14 @@
+/**
+ * Result returned by the slideout when popped.
+ * @author molinet
+ */
+export interface SlideOutStackResult {
+  /**
+   * Data returned by the slideout when popped.
+   */
+  data?: any;
+  /**
+   * Role returned by the slideout when popped.
+   */
+  role?: string;
+}
